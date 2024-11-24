fec2f76d0d4f17b0bf16e1b7e49857400eee6349
user <jankes123321@gmail.com>
2024-11-25 00:53:23 +0100

    added models and context to solution

---
+++ FitBalanceBlazor/Shared/Shared/Rodzaj.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitBalanceBlazor;
+
+public partial class Rodzaj
+{
+    [Key]
+    public int id_rodzaj { get; set; }
+
+    [Column(TypeName = "text")]
+    public string nazwa { get; set; } = null!;
+
+    [InverseProperty("rodzajNavigation")]
+    public virtual ICollection<Dieta> Dieta { get; set; } = new List<Dieta>();
+}
