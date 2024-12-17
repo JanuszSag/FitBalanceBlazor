5c9ae565b38fcd66d28590af01d1720af5bb2823
user <jankes123321@gmail.com>
2024-12-17 17:45:33 +0100

    modified meal endpoint to retrieve more data

--- FitBalanceBlazor/Shared/Shared/DietaDTO.cs
+++ FitBalanceBlazor/Shared/Shared/DietaDTO.cs
@@ -10,7 +10,7 @@ public class DietaDTO
     public int Kalorycznosc { get; set; }
     public int Autor { get; set; }
     public int Rodzaj { get; set; }
-    public List<Danie>? Dania  { get; set; }
+    public List<Danie> Dania  { get; set; }
 
     public DietaDTO(int id, string nazwa, string opis, int kalorycznosc, int autor, int rodzaj, List<Danie> danie)
     {
@@ -21,15 +21,5 @@ public class DietaDTO
         Autor = autor;
         Rodzaj = rodzaj;
         Dania = danie;
-    }
-
-    public DietaDTO(int id, string nazwa, string opis, int kalorycznosc, int autor, int rodzaj)
-    {
-        this.id = id;
-        Nazwa = nazwa;
-        Opis = opis;
-        Kalorycznosc = kalorycznosc;
-        Autor = autor;
-        Rodzaj = rodzaj;
     }
 }
