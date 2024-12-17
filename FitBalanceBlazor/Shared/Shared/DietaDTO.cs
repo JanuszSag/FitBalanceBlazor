16544303d836fdf2bf61061a618e292a34d47fdd
user <jankes123321@gmail.com>
2024-12-17 15:22:31 +0100

    added list of meals to DietDTO

--- FitBalanceBlazor/Shared/Shared/DietaDTO.cs
+++ FitBalanceBlazor/Shared/Shared/DietaDTO.cs
@@ -1,3 +1,5 @@
+using FitBalanceBlazor;
+
 namespace ClassLibrary1;
 
 public class DietaDTO
@@ -8,14 +10,26 @@ public class DietaDTO
     public int Kalorycznosc { get; set; }
     public int Autor { get; set; }
     public int Rodzaj { get; set; }
+    public List<Danie>? Dania  { get; set; }
+
+    public DietaDTO(int id, string nazwa, string opis, int kalorycznosc, int autor, int rodzaj, List<Danie> danie)
+    {
+        this.id = id;
+        Nazwa = nazwa;
+        Opis = opis;
+        Kalorycznosc = kalorycznosc;
+        Autor = autor;
+        Rodzaj = rodzaj;
+        Dania = danie;
+    }
 
     public DietaDTO(int id, string nazwa, string opis, int kalorycznosc, int autor, int rodzaj)
     {
         this.id = id;
         Nazwa = nazwa;
         Opis = opis;
-        this.Kalorycznosc = kalorycznosc;
-        this.Autor = autor;
-        this.Rodzaj = rodzaj;
+        Kalorycznosc = kalorycznosc;
+        Autor = autor;
+        Rodzaj = rodzaj;
     }
 }
