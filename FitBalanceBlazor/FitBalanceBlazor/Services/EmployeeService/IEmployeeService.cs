5074797182f84e3e7a9aa41315e6fd1ddff5db0f
JanuszSag <jankes123321@gmail.com>
2025-02-03 06:54:43 +0100

    Added functionalities to admin panel

--- FitBalanceBlazor/FitBalanceBlazor/Services/EmployeeService/IEmployeeService.cs
+++ FitBalanceBlazor/FitBalanceBlazor/Services/EmployeeService/IEmployeeService.cs
@@ -6,4 +6,10 @@ public interface IEmployeeService
 {
     Task<ServiceResponse<Pracownik>> GetEmployeePracownikAsync(int id);
     Task<ServiceResponse<List<Pracownik>>> GetAllEmployeesAsync();
+    Task<ServiceResponse<bool>> AddEmployeeAsync(PracownikDTO pracownik);
+    Task<ServiceResponse<List<Adres>>> GetAllAddressesAsync();
+    Task<ServiceResponse<bool>> AddAddressAsync(AdresDTO adress);
+    Task<ServiceResponse<bool>> DeleteAddressAsync(int id);
+    Task<ServiceResponse<bool>> DeleteEmployeeAsync(int id);
+    Task<ServiceResponse<bool>> UpdateEmployeeAsync(PracownikDTO pracownik);
 }
---
+++ FitBalanceBlazor/Shared/Shared/PracownikDTO.cs
@@ -0,0 +1,16 @@
+namespace ClassLibrary1;
+
+public class PracownikDTO
+{
+    public int id_pracownik { get; set; }
+
+    public string? imie { get; set; }
+
+    public string? nazwisko { get; set; }
+
+    public string? numer_telefonu { get; set; }
+
+    public string? stanowisko { get; set; }
+
+    public int id_uzytkownik { get; set; }
+}
